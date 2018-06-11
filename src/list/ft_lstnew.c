a647d64a681c3af77e7726ae0f344b74d334894a
dfstorm <gabriel.genois@gmail.com>
2018-06-11 17:46:16 +0200

    oh no...more...

--- src/list/ft_lstnew.c
+++ src/list/ft_lstnew.c
@@ -29,7 +29,7 @@ t_list	*ft_lstnew(void const *content, size_t content_size)
 		{
 			if ((new->content = (void*) ft_memalloc(content_size)) == NULL)
 				return (NULL);
-			ft_memcpy(new->content, content, content_size);
+			ft_memcpy(&new->content, &content, content_size);
 			new->content_size = content_size;
 		}
 		else
