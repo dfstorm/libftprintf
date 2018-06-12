fb6fec81bc8704a5bfec55aca6a997bb3afd28ec
dfstorm <gabriel.genois@gmail.com>
2018-06-12 13:17:42 +0200

    one more 14

--- src/memory/ft_memcpy_ref.c
+++ src/memory/ft_memcpy_ref.c
@@ -4,10 +4,12 @@ void	*ft_memcpy_ref(void **dst, const void **src, size_t n)
 {
 	char *odst;
 	char *osrc;
+	size_t x;
 
+	x = -1;
 	odst = (char*)(*dst);
 	osrc = (char*)(*src);
-	while (n--)
-		*(odst++) = *(osrc++);
-	return (dst);
+	while (++x < n)
+		odst[x] = osrc[x];
+	return (*dst);
 }
